ba4c4164561dafb66885aeea731d3442eaf27808
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-08-13 07:59:42 +0530

    Update Convolution.java

--- Convolution.java
+++ Convolution.java
@@ -3,3 +3,14 @@
 CONVOLUTION DAY : PERFORM LINEAR & CIRCULAR CONVOLUTION ON INPUT SIGNALS
 
 */
+class Convolution{
+  
+  static BufferedReader br = new BufferedReader
+  
+  public static void main(String[] args){
+    
+    
+  }
+  
+  
+}

bf8934e7a40407e3f8347d1c564a1649783286c6
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-08-25 08:15:53 +0530

    Update convolution.js

--- convolution.js
+++ convolution.js
@@ -1,1 +1,10 @@
 
+var countryApp = angular.module('countryApp', []);
+countryApp.controller('CountryCtrl', function ($scope){
+  $scope.countries = [
+    {"name": "China", "population": 1359821000},
+    {"name": "India", "population": 1205625000},
+    {"name": "United States of America","population": 312247000}
+  ];
+});
+
